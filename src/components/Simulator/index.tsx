7eba93d5a2bcc8e6eef71774731cf1d854dbf6d3
Thaljani Ahmed <theljaniahmed@gmail.com>
2021-01-02 21:58:12 +0100

    add frais km

--- src/components/Simulator/index.tsx
+++ src/components/Simulator/index.tsx
@@ -7,23 +7,119 @@ export default function Simulator() {
     const [portagePercent, setPortagePercent] = useState(6);
     const [mutual, setMutual] = useState(0);
     
-    // const [motorized, setMotorized] = useState('NON');
-    // const [km, setKm] = useState(0);
-    // const [power, setPower] = useState(0);
+    const [motorized, setMotorized] = useState('NON');
+    const [km, setKm] = useState(0);
+    const [fiscalPower, setFiscalPower] = useState(0);
+
+    const getWorkingDays = () => {
+        return leaveDays === 20 ? 20: 19;
+    }
 
     const computeBudget = () => {
-        const workingDays = leaveDays === 20 ? 20: 19;
-        return workingDays * tjm;
+        return getWorkingDays() * tjm;
     }
 
     const computePortageFees = () => {
-        const workingDays = leaveDays === 20 ? 20: 19;
-
-        return workingDays * tjm * portagePercent / 100;
+        return getWorkingDays() * tjm * portagePercent / 100;
     }
 
+    const computeKmFees = () => {
+        const kmPerMonth = getWorkingDays() * km;
+        let kmfees = 0;
+
+        if (fiscalPower <= 3) {
+            if (kmPerMonth <= 5000) {
+                kmfees = kmPerMonth * 0.456;
+            }
+
+            if (kmPerMonth > 5000 && kmPerMonth <= 20000) {
+                kmfees = (kmPerMonth * 0.270) + 915;
+            }
+
+            if (kmPerMonth > 20000) {
+                kmfees = kmPerMonth * 0.318;
+            }
+
+            return kmfees;
+        }
+
+        if (fiscalPower === 4) {
+            if (kmPerMonth <= 5000) {
+                kmfees = kmPerMonth * 0.523;
+            }
+
+            if (kmPerMonth > 5000 && kmPerMonth <= 20000) {
+                kmfees = (kmPerMonth * 0.291) + 1447;
+            }
+
+            if (kmPerMonth > 20000) {
+                kmfees = kmPerMonth * 0.352;
+            }
+            
+            return kmfees;
+        }
+
+        if (fiscalPower === 5) {
+            if (kmPerMonth <= 5000) {
+                kmfees = kmPerMonth * 0.548;
+            }
+
+            if (kmPerMonth > 5000 && kmPerMonth <= 20000) {
+                kmfees = (kmPerMonth * 0.305) + 1200;
+            }
+
+            if (kmPerMonth > 20000) {
+                kmfees = kmPerMonth * 0.368;
+            }
+
+            return kmfees;
+        }
+
+        if (fiscalPower === 6) {
+            if (kmPerMonth <= 5000) {
+                kmfees = kmPerMonth * 0.574;
+            }
+
+            if (kmPerMonth > 5000 && kmPerMonth <= 20000) {
+                kmfees = (kmPerMonth * 0.32) + 1253;
+            }
+
+            if (kmPerMonth > 20000) {
+                kmfees = kmPerMonth * 0.386;
+            }
+
+            return kmfees;
+        }
+
+        if (fiscalPower >= 7) {
+            if (kmPerMonth <= 5000) {
+                kmfees = kmPerMonth * 0.601;
+            }
+
+            if (kmPerMonth > 5000 && kmPerMonth <= 20000) {
+                kmfees = (kmPerMonth * 0.337) + 1301;
+            }
+
+            if (kmPerMonth > 20000) {
+                kmfees = kmPerMonth * 0.405;
+            }
+
+            return kmfees;
+        }
+
+        return kmfees;
+    }
+    
     const computeSalary = () => {
-        return ((computeBudget() - computePortageFees() - mutual ) / 1.82).toFixed(2);
+        const workingDays = leaveDays === 20 ? 20: 19;
+        const kmFees = computeKmFees();
+        const budget = workingDays * tjm;
+        const portageFees = (workingDays * tjm * portagePercent / 100);
+
+        const brutSalary = Number(budget) - Number(portageFees) - Number(mutual) - Number(kmFees);
+        const netSalary = (brutSalary/1.82).toFixed(2);
+        const totalSalarty = Number(netSalary) + Number(kmFees);
+        return totalSalarty.toFixed(2);
     }
     return (
       <div className="Simulator">
@@ -111,7 +207,7 @@ export default function Simulator() {
                         </optgroup>
                     </select>
                 </div>
-                {/* <div className='Simulator-form-step-input'>
+                <div className='Simulator-form-step-input'>
                     <span>Êtes vous motorisé?</span>
                     <select  value={motorized} onChange={e => setMotorized(e.target.value)}>
                         <optgroup>
@@ -130,10 +226,10 @@ export default function Simulator() {
                         min='0'/>
 
                         <span>Puissance fiscale</span>
-                        <input type="number" value={power} onChange={e => setPower(Number(e.target.value))} 
+                        <input type="number" value={fiscalPower} onChange={e => setFiscalPower(Number(e.target.value))} 
                         min='0'/>
                     </div>}
-                </div> */}
+                </div>
             </div>
 
             <div className="Simulator-form-step">
@@ -148,16 +244,16 @@ export default function Simulator() {
 
                 <div className='Simulator-form-step-input'>
                     <span>Chiffre d'affaires mensuel</span>
-                    <input type="number" value={computeBudget()} min='0' disabled={true}/>
+                    <input type="text" value={computeBudget()} min='0' disabled={true}/>
                 </div>
 
                 <div className='Simulator-form-step-input'>
                     <span>Frais de portage mensuel</span>
-                    <input type="number" value={computePortageFees()} min='0' disabled={true}/>
+                    <input type="text" value={computePortageFees()} min='0' disabled={true}/>
                 </div> 
                 <div className='Simulator-form-step-input'>
                     <span>Salaire Total</span>
-                    <input type="number" value={computeSalary()} min='0' disabled={true}/>
+                    <input type="text" value={computeSalary()} min='0' disabled={true}/>
                 </div> 
             </div>
           </div>
